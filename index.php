e5d78a567cd94a112932b4d5567417f630e4617d
Tyrone <tyrone@clearmindai.com>
2025-04-26 17:31:18 +0800

    general update basixs 2.1

--- index.php
+++ index.php
@@ -9,10 +9,24 @@ include("_frontend/core/fe.php");
 
 $mainpage = mainpage;
 
+function basixs_param_getter($param){
+    if($param != "" && $param != null){
+        $param = explode("&", $param);
+        foreach($param as $p){
+            $pp = explode("=", $p);
+            if(count($pp) == 2){
+                $_GET[$pp[0]] = $pp[1];
+            } 
+        }
+    }
+}
 
 
 $bee = $_GET['be'] ?? false;
 if($bee){
+    $bb = explode("?", $bee);
+    $bee = $bb[0];
+    $param = isset($bb[1]) ? $bb[1] : "";
     $bee = substr($bee, -4)==".php" ? $bee : $bee.".php";
     if(!file_exists("_backend/_routes/$bee")) {
        echo json_encode([
@@ -26,18 +40,29 @@ if($bee){
         echo json_encode([
             "code" => 404,
             "status" => "error",
-            "message" => "Backend File $bee not found",
+            "message" => "Backend File $bee not a file",
             "data" => []
         ]);exit;
     }
+    basixs_param_getter($param);
     include("_backend/core/be.php");
-    include("_backend/config/auto.php");
+    $folder_to_bee = '_backend/auto';
+
+    foreach (glob($folder_to_bee . '/*.php') as $filename) {
+        include_once $filename;
+    }
+
     include("_backend/_routes/$bee");exit;
 }
 
 
 $get = $_GET['page'] ?? false;
+$folder_to_fee = '_frontend/auto';
+
 if ($get) {
+    $bb = explode("?", $get);
+    $bee = $bb[0];
+    $param = isset($bb[1]) ? $bb[1] : "";
     $get = substr($get, -4)==".php" ? $get : $get.".php";
     if(!file_exists("_frontend/pages/$get")) {
         include("_frontend/errors/page404.php");exit;
@@ -45,9 +70,13 @@ if ($get) {
     if(!is_file("_frontend/pages/$get")) {
         include("_frontend/errors/page404.php");exit;
     }
+    basixs_param_getter($param);
+    foreach (glob($folder_to_fee . '/*.php') as $filename) {
+        include_once $filename;
+    }
     include("_frontend/pages/$get");exit;
 } else {
-    if($get=="" || $get==null){
+    if($get=="" || $get==null || $get == false){
         $mainpage = substr($mainpage, -4)==".php" ? $mainpage : $mainpage.".php";
         if(!file_exists("_frontend/pages/$mainpage")) {
             include("_frontend/errors/page404.php");exit;
@@ -55,18 +84,12 @@ if ($get) {
         if(!is_file("_frontend/pages/$mainpage")) {
             include("_frontend/errors/page404.php");exit;
         }
-        include("_frontend/config/auto.php");
+        foreach (glob($folder_to_fee . '/*.php') as $filename) {
+            include_once $filename;
+        }
         include("_frontend/pages/$mainpage");exit;
     }else{
-        $get = substr($get, -4)==".php" ? $get : $get.".php";
-        if(!file_exists("_frontend/pages/$get")) {
-            include("_frontend/errors/page404.php");exit;
-        }
-        if(!is_file("_frontend/pages/$get")) {
-            include("_frontend/errors/page404.php");exit;
-        }
-        include("_frontend/config/auto.php");
-        include("_frontend/pages/$get");exit;
+        die("Page not found!");
     }
 }
 
