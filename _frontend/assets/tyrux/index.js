d667812963d709812b34dc43d6da8eddc490aab7
Tyrone <tyrone@clearmindai.com>
2025-08-08 15:04:28 +0800

    v1.0.33

--- _frontend/assets/tyrux/index.js
+++ _frontend/assets/tyrux/index.js
@@ -20,7 +20,23 @@ function tyrux(request){
     api.request(request);
 }
 
+function get_form_data(selector) {
+    let form = null;
+    if (selector.charAt(0) === "#" || selector.charAt(0) === ".") {
+        form = document.querySelector(selector);
+    } else {
+        form = document.querySelector(`#${selector}`);
+    }
+    if (!form) return null;
+    const formData = new FormData(form);
+    const dataObject = {};
+    formData.forEach((value, key) => {
+        dataObject[key] = value;
+    });
+    return dataObject;
+}
 
 window.tyrux = tyrux;
+window.get_form_data = get_form_data;
 window.baseURL = baseURL;
 window.baseRoute = baseRoute;
