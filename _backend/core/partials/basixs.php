795c1e9306c4d00bd4d0f1babe5a9f7b30a9f7b3
Tyrone <tyrone@clearmindai.com>
2025-08-08 14:46:42 +0800

    v1.0.32

--- _backend/core/partials/basixs.php
+++ _backend/core/partials/basixs.php
@@ -1,16 +1,17 @@
 <?php
 //CodeYro
-function encrypt($data, string $key = null) {
-    if($data == null || $data == ""){
+function encrypt($data, string $key = null)
+{
+    if ($data == null || $data == "") {
         return null;
     }
     $cipher = "AES-256-CBC";
     $iv_length = openssl_cipher_iv_length($cipher);
     $iv = openssl_random_pseudo_bytes($iv_length);
     $encrypted_data = null;
-    if($key==null||$key==""){
+    if ($key == null || $key == "") {
         $encrypted_data = openssl_encrypt($data, $cipher, getenv("encrypt_key"), 0, $iv);
-    }else{
+    } else {
         $encrypted_data = openssl_encrypt($data, $cipher, $key, 0, $iv);
     }
 
@@ -19,27 +20,28 @@ function encrypt($data, string $key = null) {
     $encrypted_data = base64_encode($combined_data);
 
     $encrypted_data = strtr($encrypted_data, [
-        '+' => '-',  
-        '/' => '_',  
-        '=' => '',   
-        '&' => '%26', 
+        '+' => '-',
+        '/' => '_',
+        '=' => '',
+        '&' => '%26',
         '#' => '%23',
     ]);
     return $encrypted_data;
 }
 
-function decrypt($encrypted_data, string $key = null) {
-    if($encrypted_data == null || $encrypted_data == ""){
+function decrypt($encrypted_data, string $key = null)
+{
+    if ($encrypted_data == null || $encrypted_data == "") {
         return null;
     }
     $cipher = "AES-256-CBC";
     $iv_length = openssl_cipher_iv_length($cipher);
 
     $encrypted_data = strtr($encrypted_data, [
-        '-' => '+',  
-        '_' => '/', 
-        '%26' => '&', 
-        '%23' => '#', 
+        '-' => '+',
+        '_' => '/',
+        '%26' => '&',
+        '%23' => '#',
     ]);
 
     $padding_needed = 4 - (strlen($encrypted_data) % 4);
@@ -69,21 +71,32 @@ function decrypt($encrypted_data, string $key = null) {
 }
 
 
-function BasixsErrorException($e, $bee){
+function BasixsErrorException($e, $bee)
+{
     $arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        shuffle($arr);
-        $trace = $e->getTrace();
-        $traceString = $e->getTraceAsString();
-        $line = $e->getLine();
-        $file = $e->getFile();
-        $message = $e->getMessage();
-        $randint = $arr[0] . $arr[1] . $arr[2] . $arr[3] . $arr[4];
-        $thisdate = date("mdHis");
-        $hascode = $randint . $thisdate;
-        $code = $e->getCode();
-        $getMessage =  $message . " at line $line in $file";
-        $msg = $message . " at line $line in BE: $bee";
-        $type = get_class($e);
+    shuffle($arr);
+    $trace = $e->getTrace();
+    $traceString = $e->getTraceAsString();
+    $line = $e->getLine();
+    $file = $e->getFile();
+    $message = $e->getMessage();
+    $randint = $arr[0] . $arr[1] . $arr[2] . $arr[3] . $arr[4];
+    $thisdate = date("mdHis");
+    $hascode = $randint . $thisdate;
+    $code = $e->getCode();
+    $getMessage =  $message . " at line $line in $file";
+    $msg = $message . " at line $line in BE: $bee";
+    $type = get_class($e);
+    $err = [];
+    $env = getenv("environment") == null ? "dev" : getenv("environment");
+    if (strtolower($env) == "prod" || strtolower($env) == "production" || strtolower($env) == "uat") {
+        $err = [
+            "code" => getenv("backend_error_code"),
+            "status" => "error",
+            "message" => $message,
+            "data" => []
+        ];
+    } else {
         $err = [
             "code" => getenv("backend_error_code"),
             "status" => "error",
@@ -92,13 +105,11 @@ function BasixsErrorException($e, $bee){
             "type" => $type,
             "error_code" => $hascode,
             "error_message" => $getMessage,
-            "msg" => $msg,
-            "message" => $message,
+            "msg" => $message,
+            "message" => $msg,
             "data" => []
         ];
-        add_sql_log($getMessage, "be_errors", $hascode . " " . $bee);
-        return $err;
+    }
+    add_sql_log($getMessage, "be_errors", $hascode . " " . $bee);
+    return $err;
 }
-
-
-?>
