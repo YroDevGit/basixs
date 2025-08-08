f83a2345e66e92ec4b5d76472bddb748e528b48c
Tyrone <tyrone@clearmindai.com>
2025-08-08 18:35:40 +0800

    v1.0.36

--- _frontend/assets/tyrux/lib/tyrux.js
+++ _frontend/assets/tyrux/lib/tyrux.js
@@ -9,15 +9,14 @@ export class Tyrux {
         "Content-Type": "application/x-www-form-urlencoded"
     };
     #baseURL = "";
-
     #config = {};
 
-    constructor(config={}) {
-        if(config.headers){
+    constructor(config = {}) {
+        if (config.headers) {
             let headers = config.headers;
             this.#defaultHeaders = { ...this.#defaultHeaders, ...headers };
         }
-        if(config.baseURL && config.baseURL != null && config.baseURL != ""){
+        if (config.baseURL && config.baseURL != null && config.baseURL !== "") {
             this.#baseURL = config.baseURL;
         }
         this.#config = config;
@@ -26,24 +25,32 @@ export class Tyrux {
     request(options) {
         const xhr = new XMLHttpRequest();
         const method = options.method ? options.method.toUpperCase() : "GET";
-        let url = this.#baseURL+options.url;
+        let url = this.#baseURL + options.url;
         let data = null;
 
-        if (options.data && typeof options.data === 'object') {
-            const params = Object.keys(options.data)
-                .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(options.data[key])}`)
-                .join('&');
-            data = params;
-        }
+        const headers = options.headers ? { ...this.#defaultHeaders, ...options.headers } : this.#defaultHeaders;
+        const contentType = headers["Content-Type"] || "";
 
-        if (method === "GET" && data) {
-            url += (url.includes('?') ? '&' : '?') + data;
-            data = null;
+        if (options.data && typeof options.data === 'object') {
+            if (method === "GET") {
+                const params = Object.keys(options.data)
+                    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(options.data[key])}`)
+                    .join('&');
+                url += (url.includes('?') ? '&' : '?') + params;
+            } else {
+                if (contentType.includes("application/json")) {
+                    data = JSON.stringify(options.data);
+                } else if (contentType.includes("application/x-www-form-urlencoded")) {
+                    data = Object.keys(options.data)
+                        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(options.data[key])}`)
+                        .join('&');
+                } else {
+                    data = options.data;
+                }
+            }
         }
 
         xhr.open(method, url, true);
-
-        const headers = options.headers ? { ...this.#defaultHeaders, ...options.headers } : this.#defaultHeaders;
 
         for (const h in headers) {
             xhr.setRequestHeader(h, headers[h]);
@@ -51,23 +58,32 @@ export class Tyrux {
 
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
+                let responseData = xhr.responseText;
+                const respContentType = xhr.getResponseHeader("Content-Type") || "";
+
+                if (respContentType.includes("application/json")) {
+                    try {
+                        responseData = JSON.parse(xhr.responseText);
+                    } catch (e) {
+                        console.warn("Failed to parse JSON response:", e);
+                    }
+                }
+
                 if (xhr.status >= 200 && xhr.status < 300) {
-                    options.success?.(xhr.responseText, xhr);
+                    options.success?.(responseData, xhr);
                 } else {
-                    const errB = xhr.statusText;
-                    const xhrB = xhr;
-                    if(this.#config?.error){
-                        if(this.#config.error == "console"){
-                            console.error(errB);
+                    if (this.#config?.error) {
+                        if (this.#config.error === "console") {
+                            console.error(xhr.statusText, responseData);
                         }
-                        if(this.#config.error == "alert"){
-                            alert(errB);
+                        if (this.#config.error === "alert") {
+                            alert(xhr.statusText);
                         }
-                        if(this.#config.error == "log"){
-                            console.log(errB);
+                        if (this.#config.error === "log") {
+                            console.log(xhr.statusText, responseData);
                         }
                     }
-                    options.error?.(errB, xhrB);
+                    options.error?.(responseData, xhr);
                 }
             }
         };
