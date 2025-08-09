f04c9b42d5936e045b991c704fd15a6836955f0c
Tyrone <tyrone@clearmindai.com>
2025-08-09 17:29:12 +0800

    v1.0.48

--- _backend/core/classes/Hash.php
+++ _backend/core/classes/Hash.php
@@ -1,17 +1,19 @@
 <?php
+
 namespace Classes;
 
-class Hash{
+class Hash
+{
 
-    public static function encode(String $string, int $size = 16){
+    public static function encode(String $string, int $size = 16)
+    {
         $secret = getenv("hash_secret");
-        return substr(md5($secret.$string), 0, $length);
+        return substr(md5($secret . $string), 0, $size);
     }
 
-    public static function verify(String $string, String $hash){
+    public static function verify(String $string, String $hash)
+    {
         $hashed1 = self::encode($string);
         return $hashed1 === $hash;
     }
 }
-
-?>
