27faa8c0ce059b022049b46dc5957a6da20ae98a
Tyrone <tyrone@clearmindai.com>
2025-08-11 08:41:25 +0800

    67

--- _backend/core/classes/Request.php
+++ _backend/core/classes/Request.php
@@ -1,31 +1,44 @@
 <?php
+
 namespace Classes;
 
-class Request{
+class Request
+{
 
-    static function post(string $key){
+    static function post(string $key)
+    {
         return post($key);
     }
 
-    static function get(string $key){
+    static function get(string $key)
+    {
         return get($key);
     }
 
-    static function all(){
+    static function all()
+    {
         return postdata();
     }
 
-    static function input($key){
+    static function input($key)
+    {
         return self::post($key);
     }
 
-    static function headers(string|null $key = null){
-        return server_headers($key);
+    static function headers(string|null $key = null, $ucwords = true)
+    {
+        if ($key == null) {
+            return server_headers($key);
+        } else {
+            if ($ucwords) {
+                return server_headers(ucwords(strtolower($key)));
+            }
+            return server_headers($key);
+        }
     }
 
-    static function origin(){
+    static function origin()
+    {
         return $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
     }
-
 }
-?>
